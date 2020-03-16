54fff4638464d5ee5b53b9d09160d731e3b87055
adnan1naeem <adnan1naeem@gmail.com>
2020-03-16 12:48:14 +0500

    fix(packages): added packages

--- App.js
+++ App.js
@@ -1,76 +1,82 @@
 
 
+import { Button,AppRegistry, ActivityIndicator ,Platform} from 'react-native';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen'
 
-
-
-import { Button, ActivityIndicator ,Platform} from 'react-native';
- import React, { useEffect } from 'react';
- import SplashScreen from 'react-native-splash-screen'
-
+import { name as appName } from './app.json';
 import {
-	TuyaActivatorApi,
-	TuyaCoreApi,
-	TuyaHomeApi,
-	TuyaUserApi,
+   TuyaActivatorApi,
+   TuyaCoreApi,
+   TuyaDeviceApi,
+   TuyaHomeApi,
+   TuyaHomeManagerApi,
+   TuyaRoomApi,
+   TuyaUserApi,
 } from './sdk'
 export default    App=()=>{
- const [loading,setloading]=React.useState(false);
- const [datas,setData]=React.useState(false);
+const [loading,setloading]=React.useState(false);
+const [datas,setData]=React.useState(false);
 
- useEffect(() => {
-        if (Platform.OS !== 'web') {
-            SplashScreen.hide();
-        }
+useEffect(() => {
+       if (Platform.OS !== 'web') {
+           SplashScreen.hide();
+       }
 
-    });
+   });
 
- const data= async () =>{
-    setloading(true);
+const data= () =>{
+   setloading(true);
 
-    try{
-    await  TuyaCoreApi.initWithOptions({
-        appKey: 'sjr3df7agrrxjfxgtngh',
-        appSecret:'wuds48auyycyjm9fhj5mavfhjwyv57e8',
-    })
-    console.log('initWithOptions passed');
+     TuyaCoreApi.initWithOptions({
+       appKey: 'sjr3df7agrrxjfxgtngh',
+       appSecret:'wuds48auyycyjm9fhj5mavfhjwyv57e8',
+   })
+   
+   TuyaUserApi.loginWithUid({
+       countryCode:'92',
+       password:'34a503ccbbae87dc291f2f5c3a5ad105',
+       uid:'5d26d8f3183ce02fe4f1b465',
+   }).then((response)=>{
 
-    TuyaUserApi.loginWithUid({
-        countryCode:'92',
-        password:'34a503ccbbae87dc291f2f5c3a5ad105',
-        uid:'5d26d8f3183ce02fe4f1b465',
-    });
-    console.log('login passed');
+       console.log('initActivatorresponse',response);
+   }).catch((error)=>{
+       console.log('initActivatorerror',error);
+   })
 
-    await  TuyaActivatorApi.initActivator({
-        homeId: '10372895',
-        password:'Orient@Ogc',
-        ssid: 'BlueEast',
-        time: 50,
-        type: 'TY_EZ', // "TY_AP"
-    })
+     TuyaActivatorApi.initActivator({
+       homeId: '10372895',
+       password:'Orient@Ogc',
+       ssid: 'BlueEast',
+       time: 50,
+       type: 'TY_EZ', // "TY_AP"
+   }).then((response)=>{
 
-    console.log('initActivator passwed');
+       console.log('initActivatorresponse',response);
+   }).catch((error)=>{
+       console.log('initActivatorerror',error);
+   })
 
-  const data= await   TuyaHomeApi.getHomeDetail({
-        homeId: '10372895',
-    })
-    setData(data);
-    console.log('daaatat',data);
+
+    TuyaHomeApi.getHomeDetail({
+       homeId: '10372895',
+   }).then((response)=>{
+
+       setData(data);
+       console.log('getHomeDetail',response);
+   }).catch((error)=>{
+       console.log('getHomeDetailerror',error);
+   })
+
+
 }
-catch(error){
-    setloading(false);
 
-console.log('error',error);
-}
+   return(
+       <>
 
-}
-
-    return(
-        <>
-
-      { loading===true? <ActivityIndicator/>:<Button title='press me' onPress={data}/>}
+     { loading===true? <ActivityIndicator/>:<Button title='press me' onPress={data}/>}
 {data ?JSON.stringify(data):null}
-        
+       
 </>        
-        );
+       );
 }
